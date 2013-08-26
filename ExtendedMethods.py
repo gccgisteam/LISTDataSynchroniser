333f0d1f511da111bccdf28d9323e436dcafd5ee
Alex Leith <alexgleith+github@gmail.com>
2013-08-26 13:18:31 +1000

    First Commit
    It works!

---
+++ ExtendedMethods.py
@@ -0,0 +1,101 @@
+####=================####
+#Author: Alex Leith
+#Date: 26/08/13
+#Version: 0.0.1
+#Purpose: To abstract some custom functionality from the main file, LISTDataSynchroniser.py
+#         It does this poorly and should probably not be copied!
+####=================####
+
+import ftplib
+import ConfigParser
+
+#Check for trigger files and if found, set up trigger.
+def processTriggers(downloadedFiles):
+	#load config
+    config = ConfigParser.ConfigParser()
+    config.read("C:\GIS\Corporate\GISAdministration\Security\LIST_ftp_script_details.txt")
+    #[listEmail]
+    emailAddressLIST = [i[1] for i in config.items("listEmail")]
+    #Lists of triggers for FME scripts.
+    fmeTrigger = 0 #FME Script Triggers
+    twTrigger = 0 #SW script trigger
+
+    fmeTriggerList = ['Private_leases.zip',    'leases.zip',    'licences.zip',    'pluc.zip',    'lga_reserves.zip',    'transport.zip']
+    TasWaterTriggerList = ['ReuseLines.zip', 'ReusePts.zip', 'SewerLines.zip', 'SewerPts.zip', 'WaterLines.zip', 'WaterPts.zip']
+
+    #Required: Buildings, Heritage, Contours 2m, Public Toilets, AssetAreas, Kerbs, Zoning, Notations, Ordinance, stormwaterpipes, stormwaterpits
+    monthlyFilesList = [\
+        "C:\GIS\Projects\External\Scheduled\LIST_ftp\Upload\Buildings",\
+        "TopographicInformation\Cultural\Heritage",\
+        "TopographicInformation\Infrastructure\BuildingPlumbing\PublicToilets",\
+        "TopographicInformation\Transport\kerbs",\
+        "LandUseAndAdministration\Planning\zoning",\
+        "LandUseAndAdministration\Planning\Notations",\
+        "TopographicInformation\Infrastructure\Stormwater\Stormwaterpipes",\
+        "TopographicInformation\Infrastructure\Stormwater\Stormwaterpits"]
+    print 'TRIGGER PROCESSOR RAN'
+    for j in downloadedFiles:
+        #monthly script
+        for k in fmeTriggerList:
+            if(k == j):
+                fmeTrigger = 1
+        #tas water script
+        for l in TasWaterTriggerList:
+            if(l == j):
+                twTrigger = 1
+            
+    if(fmeTrigger):
+        logging.info("Downloaded an FME trigger layer, triggering the FME script.")
+        os.system("C:\Scripts\FME\monthly.cmd")
+        logging.info("FME Monthly Script has been run.")
+        
+        logging.info("Zipping and Uploading Data.")
+        
+        rootDir = "C:\GIS\Corporate"
+        sentItems = []
+        
+        for uploadItem in monthlyFilesList:
+            #Set up files for monthly upload...
+            
+            destFile = uploadItem.split('\\')[-1] + ".zip"
+            sentItems.append(destFile)
+            if (not "C:\\" in uploadItem):
+                source = os.path.join(rootDir, uploadItem) + ".*"
+            else:
+                source = uploadItem + ".*"
+                
+            destination = os.path.join("C:\GIS\Projects\External\Scheduled\LIST_ftp\Upload", destFile)
+            os.system(r'"C:\Program Files\7-Zip\7z.exe" a %s %s -tzip' %(destination, source))
+        
+            fp = open(destination,'rb') # file to send
+            ftp.storbinary('STOR dpiwe_upload/Stormwaterpipes.zip', fp)
+        
+        logging.info("GCC Data uploaded to The LIST. Notifying them of datasets uploaded.")
+        
+        #email The LIST
+        subjectTextLIST = "GCC Data Supply"    
+        messageTextLIST = "Hi there \n\nWe supplied:\n"
+        for i in sentItems:
+            messageTextLIST = messageTextLIST + "* " + i + "\n"
+        messageTextLIST = messageTextLIST + "\nRegards,\n\nGCC GIS\ngis@gcc.tas.gov.au"
+        
+        sendEmail(emailAddress, emailAddressLIST, subjectTextLIST, messageTextLIST, "0")
+        logging.info("Email sent to The LIST")
+
+        ftp.quit()
+    else:
+        ftp.quit()
+
+    if(twTrigger):
+        logging.info("Downloaded a Southern Water trigger layer, triggering the FME script.")
+        os.system("C:\Scripts\FME\southernWater.cmd")
+        logging.info("SW Script has been run.")
+
+    messageText = ''
+    if(fmeTrigger):
+        messageText = messageText + "\n\nNOTE: FME script was run, data were uploaded to The LIST.\nCheck for other data to upload this month, update the Data Dictionary and notify The List.\n"
+
+    if(twTrigger):
+        messageText = messageText + "\n\nNOTE: Southern Water (WaterSewer) script was run. Update the Data Dictionary and check Exponare.\n"
+
+    return messageText
---
+++ LISTDataSynchroniser.py
@@ -0,0 +1,253 @@
+####=================####
+#Author: Alex Leith
+#Date: 26/08/13
+#Version: 1.1.0
+#Purpose: To automate the downloading and uploading of GIS data from the LIST FTP server
+#Usage: LISTDataSynchroniser.py -c "c:\Path\to\configFile.txt" (-t 1) the () bit is optional 
+#       and will run the method 'processTriggers' from the ExtendedMethods file..
+####=================####
+
+##Import statements (could be pared down a bit)
+import logging, os.path, time
+from ftplib import FTP
+import smtplib
+from email.mime.text import MIMEText
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.utils import COMMASPACE
+from email import Encoders
+import types
+import socket #used to catch ftp error
+import ConfigParser
+from optparse import OptionParser #parse command line arguments
+import ExtendedMethods #a file full of custom methods including triggers.
+import zipfile
+
+#handle command line arguments
+processTriggersScript = 0;
+configFile = "LIST_ftp_script_details.txt"
+parser = OptionParser()
+parser.add_option("-c", "--config", dest="configFile",
+                  help="File to load configuration from", metavar="FILE")
+parser.add_option("-t", "--triggers", dest="processTriggersScript",
+                  help="True or false for processing the triggers script", metavar="FILE")
+(options, args) = parser.parse_args()
+if(options.configFile):
+    configFile = options.configFile
+processTriggersScript = options.processTriggersScript
+#load config
+config = ConfigParser.ConfigParser()
+config.read(configFile)
+
+print
+
+#[email]
+emailAddress = config.get("email","emailAddress")
+mailServer = config.get("email","mailServer")
+#[log]
+logFile = config.get("log","logFile")
+localPath = config.get("log","localPath")
+#[ftpdetails]
+SITE = config.get("ftpdetails","SITE")
+UN = config.get("ftpdetails","UN")
+PW = config.get("ftpdetails","PW")
+    
+#email strings
+warnText = ""
+subjectText = "FTP Download Script Run"
+messageText = ""
+triggerText = ""
+
+#some lists
+tempList = [] #temporary remote file list
+fileList = [] #remote file list, full file details
+fileNames = [] #remote file names (abstracted, e.g., southern water) this is redundant and should be removed
+fileNameTrue = [] #remote file names (actual)
+remoteFileSize = [] #remote file size
+localFileList = []
+localFileSize = []
+downloadedFiles = [] #a list of files that were downloaded
+
+##METHODS##
+
+#sends an email with one subject, message and attachment
+def sendEmail(emailFrom, emailTo, subject, message, attachment):
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['From'] = emailFrom
+    
+    if(type(emailTo) == types.ListType):
+        msg['To'] = COMMASPACE.join(emailTo)
+    else:
+        msg['To'] = emailTo
+        emailTo = [emailTo]
+    
+    msg.attach(MIMEText(message))
+    
+    if(attachment != "0"):
+        #attach the log file
+        part = MIMEBase('application', "octet-stream")
+        part.set_payload( open(logFile,"rb").read() )
+        Encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(logFile))
+        msg.attach(part)
+    
+    s = smtplib.SMTP(mailServer)
+    s.sendmail(emailFrom, emailTo, msg.as_string())
+    s.quit()
+    
+#takes input from the file listing from the ftp site and returns a list of file names (fn) and file sizes (fs)
+def getAttributes(line):
+    lineSplit = line.split(' ')
+    lineSplit = [x for x in lineSplit if x != ""]
+    
+    #file size
+    fs = lineSplit[4]
+    fnList = lineSplit[8:]
+    
+    #file name
+    fn = ""
+    for i in fnList:
+        fn = fn + i + " "
+    fn = fn.strip()
+    
+    return fn,fs
+
+def downloadFile(localFile, remoteFile):
+    f = open(localFile,"wb")
+
+    print "Downloading: "+remoteFile
+    ftp.retrbinary("RETR " + remoteFile, f.write)
+    f.close()
+
+    downloadedFiles.append(remoteFile)
+    logging.info("Retrieved: " + remoteFile + " to: " + localFile)
+    logging.info("Unzipping...")
+    unzip(localFile, localPath)
+    #os.system(r'"C:\Program Files\7-Zip\7z.exe" e %s -oC:\GIS\Projects\External\Scheduled\LIST_ftp -y' % localFile)
+
+#unzip files
+def unzip(zipFilePath, destDir):
+    zfile = zipfile.ZipFile(zipFilePath)
+    for name in zfile.namelist():
+        (dirName, fileName) = os.path.split(name)
+        if fileName == '':
+            # directory
+            newDir = destDir + '/' + dirName
+            if not os.path.exists(newDir):
+                os.mkdir(newDir)
+        else:
+            # file
+            fd = open(destDir + '/' + name, 'wb')
+            fd.write(zfile.read(name))
+            fd.close()
+    zfile.close()
+
+##everything else##
+
+#set up log
+logging.basicConfig(filename=logFile, format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO, filemode='w')
+logging.info('Script starts')
+#logging.basicConfig(filename='C:\GIS\Projects\GIS-Admin\FTPDL\LIST_FTP_Download.log',level=logging.INFO)
+
+#connect to FTP
+connected = 0
+try:
+    logging.info('Connecting to FTP site')
+    ftp = FTP(SITE)
+    ftp.login(UN,PW)
+    connected = 1
+except (EOFError, socket.error):
+    logging.error("FTP Connection Failed")
+    connected = 0
+
+if (connected):
+    #Find remote files
+    ftp.dir(tempList.append)
+
+    for line in tempList:    
+        if (os.path.splitext(line)[1].lower() == '.zip'):
+            fileList.append(line)            
+            fileName, fileSize = getAttributes(line)            
+            fileNameTrue.append(fileName)            
+            remoteFileSize.append(fileSize)
+            logging.debug('found remote: ' + fileName + ", file size is: " + fileSize)
+
+            fileNames.append(fileName)
+
+    #Find local files.
+    for file in os.listdir(localPath):
+        if (os.path.splitext(file)[1].lower() == '.zip'):        
+            localFileList.append(file)
+            file2 = os.path.join(localPath, file)    
+            localFileSize.append(str(os.path.getsize(file2)))
+            logging.debug('found local: ' + file + ", file size is: " + str(localFileSize[-1]))
+
+    #Download new files
+    countMatches = 0
+    for n,rFile in enumerate(fileNames):
+        matchNotFound = 1
+        for n2,localFile in enumerate(localFileList):
+            if(fileNames[n] == localFile):    
+                matchNotFound=0
+                if(remoteFileSize[n] == localFileSize[n2]):
+                    logging.debug("Found Match! Don't download file: " + fileNames[n])
+                    countMatches = countMatches + 1
+                else:                
+                    logging.info("Found Match! Add file for download: " + fileNames[n])                 
+
+                    fullLocalFile = os.path.join(localPath, fileNames[n])
+                    fullRemoteFile = fileNameTrue[n]                    
+                    downloadFile(fullLocalFile, fullRemoteFile)                 
+                    
+        if(matchNotFound):    
+            logging.info("Downloading new file: " + rFile)
+
+            fullLocalFile = os.path.join(localPath, fileNames[n])
+            fullRemoteFile = fileNameTrue[n]
+            downloadFile(fullLocalFile, fullRemoteFile)
+
+    logging.info("%i file matches found, not downloading." %countMatches)
+   
+    #process triggers, optional step
+    if(processTriggersScript):
+        triggerText = ExtendedMethods.processTriggers(downloadedFiles)
+    
+    #Determine which files are no longer on the server.
+    for i in localFileList:
+        found = 0
+        for j in fileNames:
+            if(i == j):
+                found = 1
+                break
+        if(not found):
+            logging.warning("Found local file, %s , no longer in remote directory." %i)
+            warnText = warnText + "Local file: %s no longer in remote directory.\n" %i
+
+    logging.info("Sending email")
+    messageText = "Download was successful\n%i new file(s) downloaded:" %(len(downloadedFiles))
+
+else:
+    #Script Failed to Connect to FTP
+    subjectText = "NOTICE! FTP Script Failed to Connect"
+    messageText = "Download Failed, check FTP connection manually and rerun."
+
+##email details##
+for i in downloadedFiles:
+    triggerText = messageText + "\n- %s" %i
+
+messageText += triggerText
+
+if(len(warnText) > 1):
+    messageText = messageText + "\n\n~~~Warning!~~~\n" + warnText
+
+#Send email to GIS
+sendEmail(emailAddress, emailAddress, subjectText, messageText, logFile)
+
+logging.info("Email sent")
+logging.info("Script Ends")
+
+
+
+
+
